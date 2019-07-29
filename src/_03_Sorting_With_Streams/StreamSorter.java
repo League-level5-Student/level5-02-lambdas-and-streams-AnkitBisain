9768c27580c2693ce7ddc91e54f849ef90a5b4d3
League Student <league@iMac-12.attlocal.net>
2019-07-29 15:24:08 -0700

    Finished

--- src/_03_Sorting_With_Streams/StreamSorter.java
+++ src/_03_Sorting_With_Streams/StreamSorter.java
@@ -13,10 +13,11 @@ public class StreamSorter {
 		}
 		
 		//1. Convert the Integer array to Stream object.
-		
+		Stream<Integer> intStm = Stream.of(nums);
 		//2. Use the sorted method of the stream to sort the numbers.
-		
+		Stream<Integer> sort = intStm.sorted();
 		//3. Use the forEach method with a lambda to print all the elements of the sorted Stream.
 		//   They should be in ascending order.
+		sort.forEach((c) -> System.out.println(c));
 	}
 }
